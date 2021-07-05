6cca181eb6494709f8991f60e9c9af9696b0a4dd
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-05 18:02:58 -0300

    fix: :ambulance: cars module

--- src/cars/entities/car.entity.ts
+++ src/cars/entities/car.entity.ts
@@ -1,1 +1,28 @@
-export class Car {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Car {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  rate: number;
+
+  @ManyToOne(() => User, (user) => user.car)
+  @ApiProperty({ type: () => User })
+  user: User;
+}
