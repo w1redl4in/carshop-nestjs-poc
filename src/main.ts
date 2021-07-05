35e2a1ed08d0e6fd72a9aded1c3b1517e1fc9a40
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-05 15:24:56 -0300

    feat: :sparkles: nest swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CarsModule } from './cars/cars.module';
+import { UsersModule } from './users/users.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Car Shop Swagger')
+    .setDescription('The Car Shop documentation')
+    .setVersion('1.0')
+    .addTag('users')
+    .addTag('cars')
+    .build();
+
+  const swagger = SwaggerModule.createDocument(app, options, {
+    include: [CarsModule, UsersModule],
+  });
+
+  SwaggerModule.setup('api', app, swagger);
+
   await app.listen(3000);
 }
 bootstrap();
