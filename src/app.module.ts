c4a11d4131d0ed065378d15ec25dd6a0ae61d239
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-05 15:24:40 -0300

    feat: :sparkles: cars module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
