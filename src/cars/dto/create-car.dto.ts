6b275011a7648a4c1327577cf3eff357cc582d8a
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-05 19:42:27 -0300

    feat: :sparkles: create car dto

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,1 +1,42 @@
-export class CreateCarDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class CreateCarDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  image: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsDefined()
+  price: number;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsDefined()
+  rate: number;
+}
